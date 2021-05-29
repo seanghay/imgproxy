5207d811d1fe01ee1fa4a3b81b1ee20f6316da1f
Max Holman <max@holmn.com>
2021-05-29 18:01:48 +0800

    chore: better types

--- src/utils.ts
+++ src/utils.ts
@@ -1,26 +1,27 @@
 import { createHmac } from 'crypto';
 import { FocusPoint, ImgproxySecureConfig, RGBColor } from './types';
 
-export const isRGBColor = (obj: any): obj is RGBColor => {
-  return typeof obj === 'object' && 'r' in obj && 'g' in obj && 'b' in obj;
+export const isRGBColor = (obj: RGBColor | unknown): obj is RGBColor => {
+  return (
+    typeof obj === 'object' &&
+    obj !== null &&
+    'r' in obj &&
+    'g' in obj &&
+    'b' in obj
+  );
 };
 
-export const isFocusPoint = (obj: any): obj is FocusPoint => {
-  return typeof obj === 'object' && 'x' in obj && 'y' in obj;
+export const isFocusPoint = (obj: FocusPoint | unknown): obj is FocusPoint => {
+  return typeof obj === 'object' && obj !== null && 'x' in obj && 'y' in obj;
 };
 
-export const isSecureConfig = (config: any): config is ImgproxySecureConfig => {
+export const isSecureConfig = (
+  config: ImgproxyConfig,
+): config is ImgproxySecureConfig => {
   return 'key' in config && 'salt' in config;
 };
 
 const hexDecode = (hex: string) => Buffer.from(hex, 'hex');
-
-export const urlSafeEncode = (data: any) =>
-  Buffer.from(data, 'utf8')
-    .toString('base64')
-    .replace(/=/g, '')
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_');
 
 export const sign = (
   key: string,
